604fe3a9d0767e383d433116599d295573d49823
XU Li <xuenru@gmail.com>
2020-05-09 22:23:21 +0200

    optimization getpage

--- getpage.py
+++ getpage.py
@@ -5,6 +5,7 @@ from bs4 import BeautifulSoup
 from json import loads
 from urllib.request import urlopen
 from urllib.parse import urlencode
+from urllib.parse import unquote
 import ssl
 
 # global variable cache
@@ -56,18 +57,50 @@ def getPage(page):
             break
         href = link.get('href')
         # wiki page
-        if not href.startswith('/wiki/'):
+        if href is None or not href.startswith('/wiki/'):
             continue
+
         # remove prefix /wiki/
         uri = href[6:]
-        # avoid duplicate
-        if uri in links_10:
+
+        # remove '#'
+        if "#" in uri:
+            shape_index = uri.index("#")
+            uri = uri[:shape_index]
+
+        # decode
+        uri = unquote(uri)
+
+        # replace '_' to space
+        uri = uri.replace('_', ' ')
+
+        # check main namespace and avoid duplicate
+        if not check_main_namespace(uri) or uri in links_10:
             continue
-        # add in target list
-        # TODO decode utf-8
+
+        # add into target list
         links_10.append(uri)
         i += 1
+
     return title, links_10
+
+
+def check_main_namespace(uri):
+    """
+    check if it's the main namespace page on wikipedia french
+    list of namespace: https://fr.wikipedia.org/wiki/Aide:Espace_de_noms
+    :param uri:
+    :return:
+    """
+    if ":" in uri:
+        colon_index = uri.index(":")
+        prefix = uri[:colon_index]
+        return prefix not in ['Média', 'Spécial', 'Discussion', 'Utilisateur', 'Discussion utilisateur', 'Wikipédia',
+                              'Discussion Wikipédia', 'Fichier', 'Discussion fichier', 'MediaWiki',
+                              'Discussion MediaWiki', 'Modèle', 'Discussion modèle', 'Aide', 'Discussion aide',
+                              'Catégorie', 'Discussion catégorie']
+
+    return True
 
 
 if __name__ == '__main__':
